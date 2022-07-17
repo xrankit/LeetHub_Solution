d04ba370b7667689341b383ca44cb12d5be46988
Ankit Yadav <ankiii0071@gmail.com>
2022-07-17 12:00:48 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (94.33%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int addDigits(int num) {
+       
+        if(num<10)
+        {
+            return num;
+        }
+        
+        else
+        {
+            int sum = 0;
+            while(num>0)
+            {
+                int rem = num % 10;
+                sum = sum + rem;
+                num = num/10;
+            }
+            return addDigits(sum);
+        }
+        
+    }
+}
